6d3a3abdecda667d46c93a9d2cc1b79d506eccc6
maxjohn81 <rivoandriharisoa@gmail.com>
2026-06-16 10:56:59 +0300

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h1>
+          Hello Alida
+        </h1>
+
+        <Button variant="default" size="lg">
+          Bonjour
+        </Button>
+
+      </div>
+
+    </>
+  );
+}
